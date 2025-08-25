74c4b0fda22222bcf651ef223b688605e917967b
Abdullah-Mahmoud557 <abdullah.salama@student.giu-uni.de>
2025-08-25 16:19:00 +0300

    refactor: remove commented console.log and tidy up code structure

--- middleware/allowedTo.js
+++ middleware/allowedTo.js
@@ -1,13 +1,14 @@
 const appError = require ('../utils/appError')
 const httpStatusText = require ('../utils/httpStatusText')
+//high order funciton is used if dynamic inputs pass 
 module.exports = (...roles) =>{
      const check = (req,res ,next ) =>{
         const role = req.currentUser.role ;
-        console.log(role);
         if (!roles.includes(role)){
             next(appError.create(`YOUR ROLE : ${role} IS NOT AUTHORIZED TO DO THIS ACTION` , 401 ,httpStatusText.FAIL))
         }
         next();
     }
+
    return check;
 }
